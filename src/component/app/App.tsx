6d345bc940648d1b00d884ed86e3ea6e6812b01f
Misha <misha.saighanov@mail.ru>
2023-06-28 11:17:26 +0600

    create popup greeting

---
+++ src/component/app/App.tsx
@@ -0,0 +1,14 @@
+
+import "./app.sass";
+
+import PopupGreeting from "../popupGreeting/PopupGreeting";
+
+function App() {
+  return (
+    <div className="app">
+      <PopupGreeting />
+    </div>
+  );
+}
+
+export default App;
